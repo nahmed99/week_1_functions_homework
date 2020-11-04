457c856aa437ceb4deb597a11e33264286a8f721
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-04 19:55:45 +0000

    some changes made for functions homework

--- python_functions_practice.py
+++ python_functions_practice.py
@@ -1,3 +1,5 @@
+import calendar
+
 def return_10():
     #pass #intentionally left empty
     return 10
@@ -13,3 +15,16 @@ def multiply(num_1, num_2):
 
 def divide(num_1, num_2):
     return num_1 / num_2
+
+def length_of_string(the_string):
+    return len(the_string)
+
+def join_string( string_1, string_2 ):
+    return string_1 + string_2
+
+def add_string_as_number( str_1, str_2 ):
+    return int(str_1) + int(str_2)
+
+
+def number_to_full_month_name( month ):
+    return calendar.month_name[month]
