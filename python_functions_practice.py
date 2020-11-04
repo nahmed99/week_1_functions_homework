0926bd856f60d2b741e3f13141bdffc3abcc2007
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-04 16:58:43 +0000

    initial file

---
+++ python_functions_practice.py
@@ -0,0 +1,15 @@
+def return_10():
+    #pass #intentionally left empty
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
